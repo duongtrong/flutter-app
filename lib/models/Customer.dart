4c667694897c12e3b56738c093c3a536d23b386f
duongtrong <trongdvd00543@fpt.edu.vn>
2020-09-28 13:06:24 +0700

    first app

---
+++ lib/models/Customer.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Customer {
+  int id;
+  String fullName;
+  String email;
+  String phoneNumber;
+  String cardNumber;
+  Image imgCardNumberBefor;
+  Image imgCardNumberAfter;
+}
